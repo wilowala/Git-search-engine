73b03fed1e4a3f913b7216ccbd26a14011628ad4
William Owalla <william.owalla@student.moringaschool.com>
2022-04-20 13:56:52 +0300

    Auto-generated commit [ci skip]

--- src/app/directive/hide-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HideDirectiveDirective } from './hide-directive.directive';
-
-describe('HideDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HideDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
